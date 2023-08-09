06e24d7dc6e9a81ae46af931d5e0ea88397a2849
Tino Michael <tino.michael.87@gmail.com>
2023-08-09 11:44:15 +0200

    use neo-tree instead of nvim-tree

--- .config/nvim/lua/tino/plugins/neo-tree.lua
+++ .config/nvim/lua/tino/plugins/neo-tree.lua
@@ -1,12 +1,52 @@
 return {
-    {
-        -- File browsing panel
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
+    -- File browsing panel
+    "nvim-neo-tree/neo-tree.nvim",
+
+    -- enabled = false,
+
+    branch = "v2.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+
+    keys = {
+        -- toggles neo-tree open/closed
+        {"<leader>b", [[:Neotree toggle<CR>]], mode="n"},
+
+        -- find and focus on current file, open tree if necessary
+        {"<M-b>", [[:Neotree reveal reveal_force_cwd<CR>]], mode="n"},
+        -- focus on tree, open if necessary
+        {"<M-m>", [[:Neotree focus<CR>]], mode="n"},
+
+        {"gs", [[:Neotree git_status float<CR>]], mode="n"},
+    },
+
+    opts = {
+        default_component_configs = {
+            git_status = {
+                symbols = {
+                  -- Change type
+                  added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                  modified  = "~", -- or "", but this is redundant info if you use git_status_colors on the name
+                  deleted   = "⨯",-- this can only be used in the git_status source
+                  renamed   = "󰁕",-- this can only be used in the git_status source
+                  -- Status type
+                  untracked = "",
+                  ignored   = "",
+                  unstaged  = "󰄱",
+                  staged    = "",
+                  conflict  = "",
+                }
+            },
+        },
+
+        window = {
+            mappings = {
+                ["o"] = "open",
+                ["e"] = "open",
+            }
         }
     }
 }
--- .config/nvim/lua/tino/plugins/nvim-tree.lua
+++ .config/nvim/lua/tino/plugins/nvim-tree.lua
@@ -1,57 +1,52 @@
 return {
-    {
-        -- File browsing panel
-        'nvim-tree/nvim-tree.lua',
+    -- File browsing panel
+    'nvim-tree/nvim-tree.lua',
 
-        -- enabled = false,
-        -- lazy = false,
+    enabled = false,
 
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    keys = {
+        -- toggles nvimtree open/closed
+        {"<leader>b", vim.cmd.NvimTreeToggle, mode="n"},
+
+        -- focuses on nvimtree, opens if necessary
+        {"<leader>m", vim.cmd.NvimTreeFocus, mode="n"},
+
+        -- finds the current file in nvimtree, marking it; opens tree if necessary
+        {"<M-b>", vim.cmd.NvimTreeFindFile, mode="n"},
+        -- {"<M-b>", [[: lua require('nvim-tree.api').tree.toggle({ path = vim.api.nvim_buf_get_name(0)})<cr>]], mode="n"},
+
+
+        {"<M-m>", vim.cmd.NvimTreeFocus, mode="n"},
+    },
+
+    opts = {
+        on_attach = on_attach,
+        sort_by = "case_sensitive",
+        view = {
+            adaptive_size = false,
+            number = true,
+            relativenumber = true,
         },
-
-        keys = {
-            -- toggles nvimtree open/closed
-            {"<leader>b", vim.cmd.NvimTreeToggle, mode="n"},
-
-            -- focuses on nvimtree, opens if necessary
-            {"<leader>m", vim.cmd.NvimTreeFocus, mode="n"},
-
-            -- finds the current file in nvimtree, marking it; opens tree if necessary
-            {"<M-b>", vim.cmd.NvimTreeFindFile, mode="n"},
-            {"<M-m>", vim.cmd.NvimTreeFocus, mode="n"},
-        },
-
-        opts = {
-            sort_by = "case_sensitive",
-            view = {
-                adaptive_size = true,
-                number = true,
-                relativenumber = true,
-                mappings = {
-                    -- custom_only = true,
-                    list = {
-                        { key = "+", action = "cd" },
-                    },
-                },
-            },
-            renderer = {
-                group_empty = true,
-                icons={
-                    glyphs = {
-                        folder = {
-                            arrow_closed = "⏵",
-                            arrow_open = "⏷",
-                        }
+        renderer = {
+            group_empty = true,
+            icons={
+                glyphs = {
+                    folder = {
+                        arrow_closed = "⏵",
+                        arrow_open = "⏷",
                     }
                 }
+            }
+        },
+        filters = {
+            custom = {
+                '__pycache__',
+                '.egg-info',
             },
-            filters = {
-                custom = {
-                    '__pycache__',
-                    '.egg-info',
-                },
-            },
-        }
+        },
     }
 }
