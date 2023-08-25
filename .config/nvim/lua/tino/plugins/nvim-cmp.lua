23c7aae5a388dba391edde21f72dc83fef510ca8
Tino Michael <tino.michael.87@gmail.com>
2023-08-25 13:21:31 +0200

    nvim-cmp tweaks

--- .config/nvim/lua/tino/plugins/nvim-cmp.lua
+++ .config/nvim/lua/tino/plugins/nvim-cmp.lua
@@ -19,12 +19,8 @@ return {
         local cmp = require('cmp')
         cmp.setup({
             snippet = {
-                -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
             window = {
@@ -34,19 +30,22 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                -- Accept currently selected item. Set `select` to `false` to only
+                -- confirm explicitly selected items.
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                -- { name = 'vsnip' }, -- For vsnip users.
-                { name = 'luasnip' }, -- For luasnip users.
-                -- { name = 'ultisnips' }, -- For ultisnips users.
-                -- { name = 'snippy' }, -- For snippy users.
+                { name = 'nvim_lsp', keyword_length = 1 },
+                { name = 'nvim_lsp_signature_helper' },
+                { name = 'nvim_lua', keyword_length = 2 },
             }, {
                 { name = 'buffer' },
                 { name = 'path' },
                 { name = 'git' },
+                { name = 'crates' },
+                { name = 'calc' },
             })
         })
     end,
