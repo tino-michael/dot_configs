87c3fbf9e047868b2d1929b3267a9906a981a639
Tino Michael <tino.michael.87@gmail.com>
2023-09-01 15:32:37 +0200

    fix breakpoint persist folder

--- .config/nvim/lua/tino/plugins/nvim-dap.lua
+++ .config/nvim/lua/tino/plugins/nvim-dap.lua
@@ -11,7 +11,7 @@ return {
         {
             'Weissle/persistent-breakpoints.nvim',
             opts = {
-                save_dir = vim.fn.stdpath('data') .. '/nvim_checkpoints',
+                save_dir = vim.fn.stdpath('data') .. '/dap_breakpoints/',
                 -- when to load the breakpoints? "BufReadPost" is recommanded.
                 load_breakpoints_event = "BufReadPost" ,
                 -- record the performance of different function. run :lua require('persistent-breakpoints.api').print_perf_data() to see the result.
