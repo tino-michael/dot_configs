979193d0300ce1c2252f8c3b1883527053199619
Tino Michael <tino.michael.87@gmail.com>
2024-04-10 12:56:02 +0200

    nvim: catppuccin theme

--- .config/nvim/lua/tino/plugins/color.lua
+++ .config/nvim/lua/tino/plugins/color.lua
@@ -2,7 +2,13 @@ return {
     'chriskempson/base16-vim',
 
     -- color scheme
-    {'dracula/vim', as = 'dracula'},
-    {'rose-pine/neovim', as = 'rose-pine'},
     {'catppuccin/vim', as = 'catppuccin'},
+
+    priority = 1000,
+
+    opt = function()
+        color = "catppuccin_mocha"
+        vim.cmd.colorscheme(color)
+        vim.cmd[[highlight ColorColumn ctermbg=4]]
+    end
 }
